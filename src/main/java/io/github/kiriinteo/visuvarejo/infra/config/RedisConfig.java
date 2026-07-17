da5dc67fde7a165d7008dc0617274577e239f3d4
Eduardo Silva Pinheiro Teodoro <eduteodoro54@gmail.com>
2026-07-17 14:26:44 -0300

    feat: Redis corrigido e implementado

--- src/main/java/io/github/kiriinteo/visuvarejo/infra/config/RedisConfig.java
+++ src/main/java/io/github/kiriinteo/visuvarejo/infra/config/RedisConfig.java
@@ -1,27 +1,24 @@
 package io.github.kiriinteo.visuvarejo.infra.config;
 
-import java.time.Duration;
-
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-@EnableCaching
 public class RedisConfig {
 
     @Bean
-    public RedisCacheManager cacheManager (RedisConnectionFactory factory){
-        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
-            .entryTtl(Duration.ofMinutes(10))
-            .disableCachingNullValues();
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
 
-        return RedisCacheManager.builder(factory)
-            .cacheDefaults(config)
-            .build();
+        template.setKeySerializer(new StringRedisSerializer());
+        
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        
+        return template;
     }
-    
 }
